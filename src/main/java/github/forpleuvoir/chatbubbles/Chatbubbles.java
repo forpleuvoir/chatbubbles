cf86c8296ddd4ec6ec7203db2a781c259e9bc6f0
forpleuvoir <cao/4321>
2020-11-06 20:44:41 +0800

    Initial commit

---
+++ src/main/java/github/forpleuvoir/chatbubbles/Chatbubbles.java
@@ -0,0 +1,12 @@
+package github.forpleuvoir.chatbubbles;
+
+/**
+ * @author forpleuvoir
+ * @project_name chatbubbles
+ * @package github.forpleuvoir.chatbubbles
+ * @class_name Chatbubbles
+ * @create_time 2020/11/6 20:08
+ */
+
+public class Chatbubbles {
+}
